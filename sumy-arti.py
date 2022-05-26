45f4a33235a2993870af56a0db12f5c4c6b35078
yusufadell <youseefadel777@gmail.com>
2022-05-26 19:44:02 +0200

    ♻️ Uses Functional B in most code portion

--- sumy-arti.py
+++ sumy-arti.py
@@ -1,37 +1,41 @@
+import requests
 from newspaper import Article
 from sumy.nlp.stemmers import Stemmer
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.summarizers.luhn import LuhnSummarizer as Summarizer
 from wand.image import Image
-import requests
 
-url = "https://www.wired.com/story/researchers-made-ultracold-quantum-bubbles-on-the-space-station/"
-article = Article(url)
-article.download()
-article.parse()
+article_URL = "https://www.wired.com/story/researchers-made-ultracold-quantum-bubbles-on-the-space-station/"
+image_URL = "https://media.wired.com/photos/628d7932616e0f74943c26c4/master/w_120,c_limit/cal-cygnus-scaled-science.jpg"
 
-LANGUAGE = "english"
-SENTENCES_COUNT = 10
 
-parser = PlaintextParser.from_string(article.text, Tokenizer(LANGUAGE))
-stemmer = Stemmer(LANGUAGE)
-summarizer = Summarizer(stemmer)
+def parse_summerize_article(url):    
+    article = Article(url)
+    article.download()
+    article.parse()
 
-CAPTION = [sentence._text for sentence in summarizer(
-    parser.document, SENTENCES_COUNT)]
+    LANGUAGE = "english"
+    SENTENCES_COUNT = 10
 
-print(CAPTION)
+    parser = PlaintextParser.from_string(article.text, Tokenizer(LANGUAGE))
+    stemmer = Stemmer(LANGUAGE)
+    summarizer = Summarizer(stemmer)
 
-image_url = 'https://media.wired.com/photos/628d7932616e0f74943c26c4/master/w_120,c_limit/cal-cygnus-scaled-science.jpg'
-image_blob = requests.get(image_url)
-with Image(blob=image_blob.content) as img:
-    print(img.size)
+    return [sentence._text for sentence in summarizer(
+        parser.document, SENTENCES_COUNT)]
 
-dims = (1080, 1920)
-ideal_width = dims[0]
-ideal_height = dims[1]
-ideal_aspect = ideal_width / ideal_height
+
+CAPTION = parse_summerize_article(article_URL)
+assert CAPTION
+
+
+def get_image_blob(url):
+    return requests.get(image_URL)
+
+
+image_blob = get_image_blob(image_URL)
+
 
 with Image(blob=image_blob.content) as img:
     size = img.size
@@ -40,28 +44,45 @@ width = size[0]
 height = size[1]
 aspect = width/height
 
-if aspect > ideal_aspect:
+# ideal Height, ideal Width
+dims = (1080, 1920)
+ideal_aspect = dims[0] / dims[1]
+
+
+def crop_edges(ideal_aspect, width, height):
     # crop the left and right edges:
     new_width = int(ideal_aspect * height)
-    offset = (width - new_width) / 2
     resize = (
         (0, 0, int(new_width), int(height)),
         (int(width-new_width), 0, int(width), int(height))
     )
-else:
-    # crop the top and bottom edges:
+    return resize
+
+
+def crop_top_bottom(ideal_aspect, width, height):
     new_height = int(width / ideal_aspect)
-    offset = (height - new_height) / 2
     resize = (
         (0, 0, int(width), int(new_height)),
         (0, int(height-new_height), int(width), int(height))
     )
 
+    return resize
+
+
+def crop_image_handler(aspect, width, height):
+    if aspect > ideal_aspect:
+        return crop_edges(ideal_aspect, width, height)
+    else:
+        return crop_top_bottom(ideal_aspect, width, height)
+
+
+resize = crop_image_handler(aspect, width, height)
+assert resize
+
 with Image(blob=image_blob.content) as img:
     img.crop(*resize[0])
-    img.save(filename='cropped_1.jpg')
-    
+    img.save(filename='assets/cropped_1.jpg')
 
 with Image(blob=image_blob.content) as img:
     img.crop(*resize[1])
-    img.save(filename='cropped_2.jpg')
+    img.save(filename='assets/cropped_2.jpg')
