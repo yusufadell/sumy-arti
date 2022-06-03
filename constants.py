2bde638134625150181a66c0281bb34484f8005e
yusufadell <youssef.31057404@ics.tanta.edu.eg>
2022-06-03 02:00:49 +0200

    ♻️ CONSTANTS gethering in one module

---
+++ constants.py
@@ -0,0 +1,17 @@
+article_URL = "https://www.wired.com/story/researchers-made-ultracold-quantum-bubbles-on-the-space-station/"
+valid_images = (".jpg", ".gif", ".png", ".tga", ".jpeg", ".PNG", ".JPG",
+                ".JPEG")
+tmp_img_path = "./assets/tmp/"
+
+filtered_image_path = "./assets/images/filtered"
+
+image1_path, image2_path = "assets/images/text_overlayed_1.jpg", "assets/images/text_overlayed_1.jpg"
+
+backup_image = "assets/images/backup/backup.png"
+
+thresholdHeight = 1000
+thresholdWidth = 1000
+
+# ideal Height, ideal Width
+dims = (1080, 1920)
+ideal_aspect = dims[0] / dims[1]
